ea37e29a51934aa720ce3ca653a741f064e31361
Alexander Koufos <exoticdft@gmail.com>
2021-04-03 17:32:41 -0700

    Updates the startup services
    - Updates the service files to call `bash` directly, instead of relying on the script itself to call bash. This removes the need for permission changes to the shell scripts.
    - Removes some unneeded comments.

--- Tools/Drone/etc/ros/env.sh
+++ Tools/Drone/etc/ros/env.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-### Add this to /etc/ros/ directory to be called every time the drone boots
 export HOSTNAME
 export ROS_HOSTNAME=$(hostname).local
 export ROS_MASTER_URI=http://node2.local:11311
