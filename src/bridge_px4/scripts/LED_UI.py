4cdb4cc0e29ad6f6ad0b6c1bc7c62b63e3e03679
lowjunen <lowjunen@gmail.com>
2021-04-12 15:56:07 -0700

    changed msg to PoseStamped

--- src/bridge_px4/scripts/LED_UI.py
+++ src/bridge_px4/scripts/LED_UI.py
@@ -5,7 +5,7 @@ import time
 import ledshim
 import smbus
 import numpy as np
-from geometry_msgs.msg import Pose 
+from geometry_msgs.msg import PoseStamped
 
 class LEDSHIM():
     def __init__(self):
@@ -14,32 +14,28 @@ class LEDSHIM():
         ledshim.display.i2c = smbus.SMBus(5)
         ledshim.set_brightness(0.6)
 
-        # ROS related things
-        rospy.init_node("led")
-        rospy.loginfo_once("Starting LED node")
-
         self.time_window = rospy.get_param('~time_window')
         self.rate_threshold = rospy.get_param('~rate_threshold')
-        self.time_stamp_array = np.zeros(self.time_window)
+        self.time_stamp_array = np.random.rand(self.time_window)
 
         ns = rospy.get_namespace()
         vision_topic = "{}mavros/vision_pose/pose".format(ns)
         rospy.loginfo_once("Topic to subscribe: {}".format(vision_topic))
 
-        rospy.Subscriber(vision_topic, Pose, self.pose_callback)
+        rospy.Subscriber(vision_topic, PoseStamped, self.pose_callback)
         rospy.Timer(rospy.Duration(0.01), self.led_update)
 
     # callback function for time stamps
     def pose_callback(self, pose_data):
-        print(pose_data)
         header = pose_data.header
         self.time_stamp_array[1:-1] = self.time_stamp_array[0:-2]
-        self.time_stamp_array[0] = rospy.Time(header.stamp.secs, header.stamp.nsecs)
+        self.time_stamp_array[0] = rospy.Time(header.stamp.secs, header.stamp.nsecs).to_sec()
         rospy.loginfo("Seconds: {}".format(self.time_stamp_array[0]))
 
     # callback for LED loop
     def led_update(self, event):
         rate_in = self.compute_rate()
+        #rospy.loginfo(rate_in)
         self.show_graph(rate_in)
 
     # computes a moving average of the frequency between messages published
@@ -59,14 +55,14 @@ class LEDSHIM():
                 r, g, b = 0, 255, 0
             else:
                 r, g, b = 255, 0, 0            
-                #r, g, b = [int(min(v, 1.0) * c) for c in [r, g, b]]
             ledshim.set_pixel(x, r, g, b)
         ledshim.show()
 
 
 if __name__ == "__main__":
-    try:
-        LEDSHIM()
-        rospy.spin()
-    except rospy.ROSInterruptException:
-        pass
+    # ROS related things
+    rospy.init_node("led", anonymous=True)
+    rospy.loginfo_once("Starting LED node")
+
+    ledshim_class = LEDSHIM()
+    rospy.spin()
