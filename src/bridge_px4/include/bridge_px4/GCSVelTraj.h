5dc20d0c69ecb2d5ebaf5ceac398520529b2a957
Keiko <keikonagami3@gmail.com>
2021-04-30 17:58:25 -0700

    restructure to publish waypoints to command/pose and command/twist

--- src/bridge_px4/include/bridge_px4/GCSVelTraj.h
+++ src/bridge_px4/include/bridge_px4/GCSVelTraj.h
@@ -5,10 +5,7 @@
                  Multi-Robot Systems Lab (MSL), Stanford University
   Contact      : knagami@stanford.edu
   Create Time  : Apr 28, 2020.
-  Description  : Based on GCS. Publish velocity setpoint commands based on
-                 position + yaw errors from a desired trajectory (loaded
-                 through csv file) via PID control. Written for a single
-                 drone, to be run on a laptop communicating with the drone.
+  Description  : Based on GCS. Publish trajectory waypoints. 
 **************************************************************************/
 
 #ifndef __GCSVelTraj_H__
@@ -45,8 +42,8 @@ protected:
 
 private:
   // ROS variables
+  ros::Publisher     pose_sp_pub;
   ros::Publisher     vel_sp_pub;
-  ros::Subscriber    pose_curr_sub;
 
   // Trajectory Variables
   int                 n_fr;
@@ -54,43 +51,13 @@ private:
   vector<double>             t_traj;
   vector< vector<double> >   st_traj;
 
-  // Gains
-  double kp_lin;
-  double ki_lin;
-  double kd_lin;
-
-  double kp_ang;
-  double ki_ang;
-  double kd_ang;
-
-  double integral_x;
-  double integral_y;
-  double integral_z;
-  double integral_yaw;
-
-  double max_integral;
-  string drone_id;
-  string target_id;
-
-  double err_x;
-  double err_y;
-  double err_z;
-  double err_yaw;
-
-  double err_x_prev;
-  double err_y_prev;
-  double err_z_prev;
-  double err_yaw_prev;
-
   // Time Variables
   double          t_end;      // end of single trajectory
   double          t_final;    // end of total trajectory
   ros::Time       t_start;    // Start time using world clock
-  ros::Duration   t_prev;     // Previous time
 
   geometry_msgs::TwistStamped  vel_sp;
-  geometry_msgs::Vector3       vel_angular;
-  geometry_msgs::PoseStamped   pose_t_curr;
+  geometry_msgs::PoseStamped   pose_sp;
 
   // Counters and Time Variables
   int k_main;
@@ -99,8 +66,6 @@ private:
 
   // Functions
   void load_trajectory(const string &input);
-  void pose_curr_cb(const geometry_msgs::PoseStamped::ConstPtr& msg);
-  void compute_integral(double &integral_term, double prev_val, double curr_val, double dt_secs);
 };
 
 #endif
--- src/bridge_px4/src/GCSVelTraj.cpp
+++ src/bridge_px4/src/GCSVelTraj.cpp
@@ -3,14 +3,6 @@
 GCSVelTraj::GCSVelTraj()
 {
     ros::param::get("~t_final", t_final);
-    ros::param::get("~drone_id", drone_id);
-    ros::param::get("~kp_lin", kp_lin);
-    ros::param::get("~ki_lin", ki_lin);
-    ros::param::get("~kd_lin", kd_lin);
-    ros::param::get("~kp_ang", kp_ang);
-    ros::param::get("~ki_ang", ki_ang);
-    ros::param::get("~kd_ang", kd_ang);
-    ros::param::get("~max_integral", max_integral);
     ros::param::get("~traj_id", traj_id);
 
     load_trajectory(traj_id);
@@ -21,43 +13,19 @@ GCSVelTraj::GCSVelTraj()
     ROS_INFO("Trajectory Loaded");
 
     // ROS Initialization
-    string drone_topic_sub = drone_id + "/mavros/vision_pose/pose";
-    string drone_topic_pub = drone_id + "/setpoint/velocity";
-
-    pose_curr_sub = nh.subscribe(drone_topic_sub,1,&GCSVelTraj::pose_curr_cb,this);
-    vel_sp_pub = nh.advertise<geometry_msgs::TwistStamped>(drone_topic_pub,1);
+    string targetPose_topic = "command/pose";
+    string targetTwist_topic = "command/twist";
+    pose_sp_pub = nh.advertise<geometry_msgs::PoseStamped>(targetPose_topic,1);
+    vel_sp_pub = nh.advertise<geometry_msgs::TwistStamped>(targetTwist_topic,1);
 
     ROS_INFO("ROS Publishers Initialized");
 
-    // Error terms
-    err_x = 0.0;
-    err_y = 0.0;
-    err_z = 0.0;
-    err_yaw = 0.0;
-
-    err_x_prev = 0.0;
-    err_y_prev = 0.0;
-    err_z_prev = 0.0;
-    err_yaw_prev = 0.0;
-
-    // integral term
-    integral_x = 0.0;
-    integral_y = 0.0;
-    integral_z = 0.0;
-    integral_yaw = 0.0;
-
-    vel_angular.x = 0.0;
-    vel_angular.y = 0.0;
-    vel_angular.z = 0.0;
-
-    // Relevant Times
-    t_start = ros::Time::now();
-    t_prev = t_start - ros::Time::now();
-
-    // Initialize Counter
+    // Counters and Time Initialization
     k_main = 0;
     k_traj = 0;
     k_loop = 0;
+
+    t_start = ros::Time::now();
 
     ROS_INFO("Counters Initialized.");
 }
@@ -67,81 +35,42 @@ GCSVelTraj::~GCSVelTraj()
   ROS_WARN("Terminating Publisher");
 }
 
-void GCSVelTraj::pose_curr_cb(const geometry_msgs::PoseStamped::ConstPtr& msg){
-    // ROS_INFO("Pose Callback");
-    pose_t_curr = *msg;
-}
-
-void GCSVelTraj::compute_integral(double &integral_term, double prev_val, double curr_val, double dt_secs)
-{
-    ROS_INFO("Comptue Integral");
-    if ((integral_term >= max_integral && curr_val <= 0.0) ||
-        (integral_term <= -max_integral && curr_val >= 0.0) ||
-        (integral_term < max_integral && integral_term > -max_integral))     // account for windup
-    {
-        // ROS_INFO("INCREASE INTEGRAL");
-        integral_term += (prev_val + curr_val)*dt_secs/2.0;
-    }
-}
-
 void GCSVelTraj::update_setpoint()
 {
     // Time
     ros::Duration t_now = ros::Time::now() - t_start;
-    ros::Duration dt = t_now - t_prev;
     ros::Duration t_wp = ros::Duration(t_traj[k_traj]);
-    t_prev = ros::Time::now() - t_start;
-    double dt_secs = dt.toSec();
 
-    // Update Veloctiy Setpoints
     if (t_now <= ros::Duration(t_final))
     {
         ros::Duration t_loop = t_now - ros::Duration(k_loop*t_end);
+
         if ((k_traj < n_fr) && (t_loop > t_wp))
         {
-            // Compute position errors
-            err_x = st_traj[0][k_traj] - pose_t_curr.pose.position.x;
-            err_y = st_traj[1][k_traj] - pose_t_curr.pose.position.y;
-            err_z = st_traj[2][k_traj] - pose_t_curr.pose.position.z;
+            pose_sp.pose.position.x = st_traj[0][k_traj];
+            pose_sp.pose.position.y = st_traj[1][k_traj];
+            pose_sp.pose.position.z = st_traj[2][k_traj];
 
-            // Compute yaw error
-            double qw = pose_t_curr.pose.orientation.w;
-            double qx = pose_t_curr.pose.orientation.x;
-            double qy = pose_t_curr.pose.orientation.y;
-            double qz = pose_t_curr.pose.orientation.z;
-            double yaw_curr = atan2(2*(qw*qz + qx*qy), 1 - 2*(qy*qy + qz*qz));
-            err_yaw = st_traj[3][k_traj] - yaw_curr;
+            double roll = 0.0f;
+            double pitch = 0.0f;
+            double yaw = st_traj[3][k_traj];
 
-            // Compute integral terms
-            compute_integral(integral_x, err_x_prev, err_x, dt_secs);
-            compute_integral(integral_y, err_y_prev, err_y, dt_secs);
-            compute_integral(integral_z, err_z_prev, err_z, dt_secs);
-            compute_integral(integral_yaw, err_yaw_prev, err_yaw, dt_secs);
+            double cy = cos(yaw * 0.5);
+            double sy = sin(yaw * 0.5);
+            double cp = cos(pitch * 0.5);
+            double sp = sin(pitch * 0.5);
+            double cr = cos(roll * 0.5);
+            double sr = sin(roll * 0.5);
 
-            // Velocity Setpoints
-            vel_sp.twist.linear.x = kp_lin*err_x
-                               + ki_lin*integral_x
-                               + kd_lin*(err_x_prev - err_x)/dt_secs;
+            pose_sp.pose.orientation.w = cr * cp * cy + sr * sp * sy;
+            pose_sp.pose.orientation.x = sr * cp * cy - cr * sp * sy;
+            pose_sp.pose.orientation.y = cr * sp * cy + sr * cp * sy;
+            pose_sp.pose.orientation.z = cr * cp * sy - sr * sp * cy;
 
-            vel_sp.twist.linear.y = kp_lin*err_y
-                               + ki_lin*integral_y
-                               + kd_lin*(err_y_prev - err_y)/dt_secs;
-
-            vel_sp.twist.linear.z = kp_lin*err_z
-                               + ki_lin*integral_z
-                               + kd_lin*(err_z_prev - err_z)/dt_secs;
-
-            vel_sp.twist.angular = vel_angular;
-
-            vel_sp.twist.angular.z = kp_ang*err_yaw
-                                   + ki_ang*integral_yaw
-                                   + kd_ang*(err_yaw_prev - err_yaw)/dt_secs;
-
-            // Assign previous time step errors
-            err_x_prev = err_x;
-            err_y_prev = err_y;
-            err_z_prev = err_z;
-            err_yaw_prev = err_yaw;
+            vel_sp.twist.linear.x = st_traj[4][k_traj];
+            vel_sp.twist.linear.y = st_traj[5][k_traj];
+            vel_sp.twist.linear.z = st_traj[6][k_traj];
+            vel_sp.twist.angular.z = st_traj[7][k_traj];
 
             k_traj++;
         }
@@ -152,11 +81,17 @@ void GCSVelTraj::update_setpoint()
         }
 
         ros::Time t_stamp = ros::Time::now();
+
+        // Publish Pose
+        pose_sp.header.stamp = t_stamp;
+        pose_sp.header.seq = k_main;
+        pose_sp.header.frame_id = "world";
+        pose_sp_pub.publish(pose_sp);
+
+        // Publish Twist
         vel_sp.header.stamp = t_stamp;
         vel_sp.header.seq = k_main;
         vel_sp.header.frame_id = "world";
-
-        // Publish
         vel_sp_pub.publish(vel_sp);
 
         k_main++;
@@ -202,7 +137,7 @@ void GCSVelTraj::load_trajectory(const string& input)
             t_traj[i] = parsedCsv[0][i];
         }
 
-        st_traj = vector< vector<double> >(4, vector<double>(n_fr));
+        st_traj = vector< vector<double> >(8, vector<double>(n_fr));
         for (int i=1 ; i<rows ; i++) {
             for (int j=0 ; j<cols ; j++) {
                 st_traj[i-1][j] = parsedCsv[i][j];
