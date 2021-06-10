41cc47aa179a57b72e284a27a315acda0c6d14aa
Keiko <keikonagami3@gmail.com>
2021-06-10 15:25:37 -0700

    generate fake mocap

---
+++ src/bridge_px4/include/bridge_px4/mocap_gen.h
@@ -0,0 +1,47 @@
+
+#ifndef __MOCAP_GEN_NODE_H__
+#define __MOCAP_GEN_NODE_H__
+
+#include "ros/ros.h"
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#include <geometry_msgs/PoseStamped.h>
+#include <mavros_msgs/CommandBool.h>
+#include <mavros_msgs/SetMode.h>
+#include <mavros_msgs/State.h>
+
+#include <Eigen/Dense>
+
+using namespace Eigen;
+using namespace std;
+
+class MocapGen
+{
+public:
+  // Constructor
+  MocapGen();
+  virtual ~MocapGen();
+
+  // Setpoint Function(s)
+  void update_setpoint();
+
+protected:
+  ros::NodeHandle nh;
+
+private:
+  // ROS variables
+  ros::Publisher   pose_curr_pub;
+  ros::Subscriber  pose_curr_sub;
+
+  geometry_msgs::PoseStamped pose_curr_in;
+  geometry_msgs::PoseStamped pose_curr_out;
+
+  // Functions
+  void pose_sp_cb(const geometry_msgs::PoseStamped::ConstPtr& msg);
+};
+
+#endif
---
+++ src/bridge_px4/src/mocap_gen.cpp
@@ -0,0 +1,55 @@
+#include <bridge_px4/mocap_gen.h>
+
+MocapGen::MocapGen()
+{
+    // Subscribe to mavros/local_position/pose
+    pose_curr_sub = nh.subscribe("mavros/local_position/pose",1,&MocapGen::pose_sp_cb,this);
+
+    // Publish to mavros/vision_pose/pose
+    string drone_topic = "mavros/vision_pose/pose";
+    pose_curr_pub = nh.advertise<geometry_msgs::PoseStamped>(drone_topic,1);
+
+    ROS_INFO("ROS Publishers Initialized");
+}
+
+MocapGen::~MocapGen()
+{
+  ROS_WARN("Terminating Publisher");
+}
+
+void MocapGen::pose_sp_cb(const geometry_msgs::PoseStamped::ConstPtr& msg){
+    pose_curr_in = *msg;
+}
+
+void MocapGen::update_setpoint()
+{
+    pose_curr_out = pose_curr_in;
+    // ROS_INFO("PUBLISH MOCAP");
+    pose_curr_pub.publish(pose_curr_out);
+
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "mocap_gen_node");
+
+    MocapGen mocap_gen;
+
+    // ROS_INFO("SET OFFBOARD");
+    // mavros_msgs::SetMode offb_set_mode;
+    // offb_set_mode.request.custom_mode = "OFFBOARD";
+    //
+    // mavros_msgs::CommandBool arm_cmd;
+    // arm_cmd.request.value = true;
+
+    ros::Rate rate(100);
+    while(ros::ok()){
+
+        mocap_gen.update_setpoint();
+
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    return 0;
+}
