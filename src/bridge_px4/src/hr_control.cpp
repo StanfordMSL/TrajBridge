905d65ebfbc313501355cee41bacfd185b714ad6
JunEn Low <lowjunen@gmail.com>
2021-08-19 14:14:34 -0700

    Think i got it.

--- src/bridge_px4/src/hr_control.cpp
+++ src/bridge_px4/src/hr_control.cpp
@@ -93,9 +93,6 @@ void HR_Control::policy_update()
         del_x = x_curr - x_bar;            
         u_br = u_curr + L_curr*del_x;
 
-        cout << del_x << endl;
-        cout << " ohoh " << endl;
-
         // Increment Counter
         k_main += 1;
     } else 
