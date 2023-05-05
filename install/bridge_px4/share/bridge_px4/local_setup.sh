ea09a1ccf663f7250256dafde0a2687e455dfdef
lowjunen <lowjunen@gmail.com>
2023-05-05 11:32:34 -0700

    Test save from drone5.

--- install/bridge_px4/share/bridge_px4/local_setup.sh
+++ install/bridge_px4/share/bridge_px4/local_setup.sh
@@ -2,7 +2,7 @@
 
 # since this file is sourced use either the provided AMENT_CURRENT_PREFIX
 # or fall back to the destination set at configure time
-: ${AMENT_CURRENT_PREFIX:="/home/lowjunen/StanfordMSL/TrajBridge/install/bridge_px4"}
+: ${AMENT_CURRENT_PREFIX:="/home/carl/StanfordMSL/TrajBridge/install/bridge_px4"}
 if [ ! -d "$AMENT_CURRENT_PREFIX" ]; then
   if [ -z "$COLCON_CURRENT_PREFIX" ]; then
     echo "The compile time prefix path '$AMENT_CURRENT_PREFIX' doesn't " \
--- install/px4_ros_com/share/px4_ros_com/local_setup.sh
+++ install/px4_ros_com/share/px4_ros_com/local_setup.sh
@@ -2,7 +2,7 @@
 
 # since this file is sourced use either the provided AMENT_CURRENT_PREFIX
 # or fall back to the destination set at configure time
-: ${AMENT_CURRENT_PREFIX:="/home/lowjunen/StanfordMSL/TrajBridge/install/px4_ros_com"}
+: ${AMENT_CURRENT_PREFIX:="/home/carl/StanfordMSL/TrajBridge/install/px4_ros_com"}
 if [ ! -d "$AMENT_CURRENT_PREFIX" ]; then
   if [ -z "$COLCON_CURRENT_PREFIX" ]; then
     echo "The compile time prefix path '$AMENT_CURRENT_PREFIX' doesn't " \
