9b0d946d1a68d7be14f917da251b2b83aa95900f
lowjunen <lowjunen@gmail.com>
2023-06-12 15:36:23 -0700

    Save to check if trajbridge is clean.

--- src/px4_ros_com/src/examples/offboard/offboard_control.cpp
+++ src/px4_ros_com/src/examples/offboard/offboard_control.cpp
@@ -42,30 +42,74 @@
 #include <px4_msgs/msg/trajectory_setpoint.hpp>
 #include <px4_msgs/msg/vehicle_command.hpp>
 #include <px4_msgs/msg/vehicle_control_mode.hpp>
+#include <px4_msgs/msg/vehicle_odometry.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <stdint.h>
+#include <eigen3/Eigen/Dense>
 
 #include <chrono>
 #include <iostream>
 
+using namespace std;
 using namespace std::chrono;
 using namespace std::chrono_literals;
 using namespace px4_msgs::msg;
+using namespace Eigen;
+
 
 class OffboardControl : public rclcpp::Node
 {
 public:
 	OffboardControl() : Node("offboard_control")
 	{
+		// Parameters
+		declare_parameter("pos_ref",vector<double>{0.0,0.0,0.0});
+		declare_parameter("safe_lim", 1.0);
+		declare_parameter("test_lim", 0.1);
 
+		vector<double> vect_pos_ref; 
+		get_parameter("pos_ref",vect_pos_ref);
+		get_parameter("safe_lim",lim_safe);
+		get_parameter("test_lim",lim_test);
+		pos_ref << vect_pos_ref[0],vect_pos_ref[1],vect_pos_ref[2];
+
+		// Checkers
+		in_safe = true;
+		in_test = false;
+
+		// Publishers
 		offboard_control_mode_publisher_ = this->create_publisher<OffboardControlMode>("/fmu/in/offboard_control_mode", 10);
 		trajectory_setpoint_publisher_ = this->create_publisher<TrajectorySetpoint>("/fmu/in/trajectory_setpoint", 10);
 		vehicle_command_publisher_ = this->create_publisher<VehicleCommand>("/fmu/in/vehicle_command", 10);
+      	
+		// Subscribers
+		rmw_qos_profile_t qos_profile = rmw_qos_profile_sensor_data;
+		auto qos = rclcpp::QoS(rclcpp::QoSInitialization(qos_profile.history, 5), qos_profile);
+
+		vehicle_odometry_subscriber_ = this->create_subscription<px4_msgs::msg::VehicleOdometry>("/fmu/out/vehicle_odometry", qos,
+		[this](const px4_msgs::msg::VehicleOdometry::UniquePtr msg) {
+			Vector3f pos_now;
+			pos_now << msg->position[0], msg->position[1], msg->position[2];
+
+			float dist = (pos_now-pos_ref).norm();
+
+			if (dist > lim_safe) {
+				OffboardControl::in_safe = false;
+				OffboardControl::in_test = false;
+			} else {
+				if (dist < lim_test) {
+					OffboardControl::in_test = true;
+				}
+			}
+
+			
+		});
 
 		offboard_setpoint_counter_ = 0;
+		offboard_out_zone_counter_ = 0;
 
 		auto timer_callback = [this]() -> void {
-
+			
 			if (offboard_setpoint_counter_ == 10) {
 				// Change to Offboard mode after 10 setpoints
 				this->publish_vehicle_command(VehicleCommand::VEHICLE_CMD_DO_SET_MODE, 1, 6);
@@ -74,9 +118,19 @@ public:
 				this->arm();
 			}
 
-			// offboard_control_mode needs to be paired with trajectory_setpoint
-			publish_offboard_control_mode();
-			publish_trajectory_setpoint();
+			if (OffboardControl::in_safe == false) {			
+				if (offboard_out_zone_counter_ < 11) {
+					this->publish_vehicle_command(VehicleCommand::VEHICLE_CMD_NAV_LAND);
+					offboard_out_zone_counter_++;
+				}
+			} else {
+				if (OffboardControl::in_test == true) {
+					this->publish_offboard_control("sfti");
+				} else {
+					this->publish_offboard_control("taxi");
+				}
+				
+			}
 
 			// stop the counter after reaching 11
 			if (offboard_setpoint_counter_ < 11) {
@@ -95,14 +149,19 @@ private:
 	rclcpp::Publisher<OffboardControlMode>::SharedPtr offboard_control_mode_publisher_;
 	rclcpp::Publisher<TrajectorySetpoint>::SharedPtr trajectory_setpoint_publisher_;
 	rclcpp::Publisher<VehicleCommand>::SharedPtr vehicle_command_publisher_;
+    rclcpp::Subscription<px4_msgs::msg::VehicleOdometry>::SharedPtr vehicle_odometry_subscriber_;
 
 	std::atomic<uint64_t> timestamp_;   //!< common synced timestamped
 
 	uint64_t offboard_setpoint_counter_;   //!< counter for the number of setpoints sent
+	uint64_t offboard_out_zone_counter_;   //!< counter for the number of setpoints sent
+	Vector3f pos_ref;
+	bool in_safe, in_test;
+	float lim_safe,lim_test;	
 
-	void publish_offboard_control_mode();
-	void publish_trajectory_setpoint();
+	void publish_offboard_control(std::string mode);
 	void publish_vehicle_command(uint16_t command, float param1 = 0.0, float param2 = 0.0);
+	void vehicle_odometry_callback(const px4_msgs::msg::VehicleOdometry msg);
 };
 
 /**
@@ -129,30 +188,44 @@ void OffboardControl::disarm()
  * @brief Publish the offboard control mode.
  *        For this example, only position and altitude controls are active.
  */
-void OffboardControl::publish_offboard_control_mode()
+void OffboardControl::publish_offboard_control(std::string mode)
 {
-	OffboardControlMode msg{};
-	msg.position = true;
-	msg.velocity = false;
-	msg.acceleration = false;
-	msg.attitude = false;
-	msg.body_rate = false;
-	msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
-	offboard_control_mode_publisher_->publish(msg);
-}
+	// Output Variables
+	OffboardControlMode msg_ocm{};
+	TrajectorySetpoint msg_tsp{};
 
-/**
- * @brief Publish a trajectory setpoint
- *        For this example, it sends a trajectory setpoint to make the
- *        vehicle hover at 5 meters with a yaw angle of 180 degrees.
- */
-void OffboardControl::publish_trajectory_setpoint()
-{
-	TrajectorySetpoint msg{};
-	msg.position = {0.0, 0.0, -5.0};
-	msg.yaw = -3.14; // [-PI:PI]
-	msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
-	trajectory_setpoint_publisher_->publish(msg);
+	// Mode Variables
+	if (mode == "taxi") {
+		msg_ocm.position = true;
+		msg_ocm.actuator  = false;
+
+		msg_tsp.position = {0.0, 0.0, -3.0};
+	} else if (mode == "sfti") {
+		msg_ocm.position = true;
+		msg_ocm.actuator  = false;
+
+		msg_tsp.position = {0.0, 0.0, -1.0};
+	} else {
+		msg_ocm.position = false;
+		msg_ocm.actuator  = false;
+
+		msg_tsp.position = {0.0, 0.0, 0.0};
+	}
+
+	// Mode Constants
+	msg_ocm.velocity = false;
+	msg_ocm.acceleration = false;
+	msg_ocm.attitude = false;
+	msg_ocm.body_rate = false;
+
+	msg_tsp.yaw = 0.0; // [-PI:PI]
+
+	msg_ocm.timestamp = msg_tsp.timestamp = this->get_clock()->now().nanoseconds() / 1000;
+
+	// Publish
+	trajectory_setpoint_publisher_->publish(msg_tsp);
+	offboard_control_mode_publisher_->publish(msg_ocm);
+
 }
 
 /**
