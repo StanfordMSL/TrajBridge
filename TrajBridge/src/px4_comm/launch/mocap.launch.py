88326336aa5155c0e911138ca7d088ebb7cbf577
JunEn Low <lowjunen@gmail.com>
2024-03-05 12:06:49 -0800

    Unified launch file for mocap node.

--- TrajBridge/src/px4_comm/launch/mocap.launch.py
+++ TrajBridge/src/px4_comm/launch/mocap.launch.py
@@ -2,12 +2,24 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    return LaunchDescription([
+    return LaunchDescription(
+        [
+        Node(
+            package='vrpn_mocap',
+            executable='client_node',
+            name='vrpn_client_0',
+            namespace='vrpn_mocap',
+            output='screen',
+            emulate_tty=True,
+            parameters=[{'server': 'mocap.local',
+                         'port': 3883}]
+            ),
         Node(
             package='px4_comm',
             executable='mocap_node.py',
             name='mocap_0',
             output='screen',
-            emulate_tty=True,
+            emulate_tty=True
+            )
+        ]
         )
-    ])
