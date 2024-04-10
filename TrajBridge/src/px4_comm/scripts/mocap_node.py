b4b306d0a05091c10796b33688225214038047e6
lowjunen <lowjunen@gmail.com>
2024-04-09 22:14:53 -0700

    Added multi-agent ros2 capabilities.

--- TrajBridge/src/px4_comm/scripts/mocap_node.py
+++ TrajBridge/src/px4_comm/scripts/mocap_node.py
@@ -4,8 +4,9 @@ import numpy as np
 
 import rclpy
 from rclpy.node import Node
+import rclpy.publisher
 from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy, DurabilityPolicy
-
+from functools import partial
 from geometry_msgs.msg import (
     PoseStamped
 )
@@ -13,6 +14,7 @@ from geometry_msgs.msg import (
 from px4_msgs.msg import (
     VehicleOdometry,
 )
+import rclpy.subscription
 
 class Mocap(Node):
     """Node for sending pose data from mocap to drone."""
@@ -21,40 +23,74 @@ class Mocap(Node):
         super().__init__('mocap_node')
 
         # Configure QoS profile for publishing and subscribing
-        qos_profile = QoSProfile(
+        self.qos_profile = QoSProfile(
             reliability=ReliabilityPolicy.BEST_EFFORT,
             durability=DurabilityPolicy.VOLATILE,
             history=HistoryPolicy.KEEP_LAST,
             depth=1
         )
 
-        # Create subscribers
-        self.mocap_pose_subscriber = self.create_subscription(
-            PoseStamped, '/vrpn_mocap/fmu/pose', self.vehicle_attitude_callback, qos_profile)
-        self.vision_pose_publisher = self.create_publisher(
-            VehicleOdometry,'/fmu/in/vehicle_visual_odometry', qos_profile)
-        
-    def vehicle_attitude_callback(self, msg:PoseStamped) -> None:
-        vo = VehicleOdometry()
+        # Create timer that looks for vrpn_mocap topics
+        self.searchLoop = self.create_timer(0.2, self.search_callback)
+        self.assets = []
+        self.subs = []
+        self.pubs = []
 
-        vo.timestamp = int(self.get_clock().now().nanoseconds / 1000)
-        vo.timestamp_sample = int(msg.header.stamp.sec * 1e6 + msg.header.stamp.nanosec / 1e3)
-        
-        vo.pose_frame = VehicleOdometry.POSE_FRAME_FRD
-        vo.position = [ msg.pose.position.x,
-                       -msg.pose.position.y,
-                       -msg.pose.position.z]
-        
-        vo.q = [ msg.pose.orientation.w,
-                 msg.pose.orientation.x,
-                -msg.pose.orientation.y,
-                -msg.pose.orientation.z]
+    def search_callback(self) -> None:
+        topic_list = self.get_topic_names_and_types()
 
-        vo.velocity_frame = VehicleOdometry.VELOCITY_FRAME_UNKNOWN
-        vo.velocity = np.array([None,None,None]).astype(np.float32)
-        vo.angular_velocity = np.array([None,None,None]).astype(np.float32)
+        for topic in topic_list:
+            text = topic[0].split('/')
 
-        self.vision_pose_publisher.publish(vo)
+            if text[1] == 'vrpn_mocap':
+                if text[2] not in self.assets:
+                    if len(self.assets) > 5:
+                        # Limit to 5 mocap topics
+                        print("Warning: More than 5 mocap topics found. Not adding more")
+                    else:
+                        # Add new mocap topic
+                        self.assets.append(text[2])
+                        print("Found mocap topic:", text[2])
+
+                        # Create subscriber for new mocap topic
+                        self.subs.append(
+                            self.create_subscription(
+                                PoseStamped,topic[0],
+                                self.make_callback(len(self.assets)-1), 
+                                self.qos_profile))
+                        
+                        # Create publisher for new mocap topic
+                        self.pubs.append(
+                            self.create_publisher(
+                                VehicleOdometry,text[2]+"/in/vehicle_visual_odometry", 
+                                self.qos_profile)
+                        )
+
+    def make_callback(self, idx:int):
+        def vehicle_attitude_callback(msg:PoseStamped) -> None:
+            vo = VehicleOdometry()
+
+            vo.timestamp = int(self.get_clock().now().nanoseconds / 1000)
+            vo.timestamp_sample = int(msg.header.stamp.sec * 1e6 + msg.header.stamp.nanosec / 1e3)
+            
+            vo.pose_frame = VehicleOdometry.POSE_FRAME_FRD
+            vo.position = [ msg.pose.position.x,
+                        -msg.pose.position.y,
+                        -msg.pose.position.z]
+            
+            vo.q = [ msg.pose.orientation.w,
+                    msg.pose.orientation.x,
+                    -msg.pose.orientation.y,
+                    -msg.pose.orientation.z]
+
+            vo.velocity_frame = VehicleOdometry.VELOCITY_FRAME_UNKNOWN
+            vo.velocity = np.array([None,None,None]).astype(np.float32)
+            vo.angular_velocity = np.array([None,None,None]).astype(np.float32)
+
+            self.pubs[idx].publish(vo)
+
+        return vehicle_attitude_callback
+    
 
 def main(args=None) -> None:
     print('Starting mocap node...')
